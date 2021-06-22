e556b3a2df1d579c52d9490695f58411a779ad75
afeiship <1290657123@qq.com>
2021-06-22 22:28:50 +0800

    feat: add test case

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -2,9 +2,20 @@
   require('../src');
 
   describe('api.basic test', () => {
-    test('nx.repeatBy', function () {
+    test('nx.repeatBy string', function () {
       var res = nx.repeatBy('a', 3);
       expect(res).toEqual(['a', 'a', 'a']);
     });
+
+    test('repeat obj', ()=>{
+      var obj = { data:'2021-06-22', time:'22:27:34'}
+      var res = nx.repeatBy(obj, 3);
+
+      expect(res).toEqual([
+        { data: '2021-06-22', time: '22:27:34' },
+        { data: '2021-06-22', time: '22:27:34' },
+        { data: '2021-06-22', time: '22:27:34' }
+      ]);
+    })
   });
 })();
